d45e2403e2fa3d8fd1df6a43ab91538fb8cd8a20
valex <valerapanow03@gmail.com>
2022-07-04 17:48:53 +0300

    createadmin command

--- Procfile
+++ Procfile
@@ -1,4 +1,5 @@
 release: python backend/manage.py makemigrations
 release: python backend/manage.py migrate
+release: python backend/manage.py createadmin
 worker: python backend/manage.py bot_run
 web: python backend/manage.py runserver 0.0.0.0:$PORT
---
+++ backend/app/management/commands/createadmin.py
@@ -0,0 +1,9 @@
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+
+User = get_user_model()
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        User.create_superuser(username='test', password='test')
