65169d71435678ce858b8e57301db6b97ede2b31
valex <valerapanow03@gmail.com>
2022-07-04 17:53:19 +0300

    createadmin command fix

--- backend/app/management/commands/createadmin.py
+++ backend/app/management/commands/createadmin.py
@@ -6,4 +6,4 @@ User = get_user_model()
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        User.objects.create_superuser(username='test', password='test')
+        User.objects.create_superuser(username='admin', password='admin')
